5004cca034578de135ee9f7b37fcb0075f8dd609
R0754975 <r0754975@student.thomasmore.be>
2022-10-16 15:51:03 +0200

    feat(Weather): add getWeather get the users weather and then store it locally for future revisits

--- src/Weather.js
+++ src/Weather.js
@@ -12,4 +12,21 @@ export default class Weather {
         }
     }
 
+    getWeather(position) {
+        console.log(position);
+        let lat = position.coords.latitude;
+        let lon = position.coords.longitude;
+
+        const url = `https://api.weatherapi.com/v1/current.json?key=${this.apiKey}&q=${lat},${lon}`;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                
+                // save to localstorage
+                localStorage.setItem('weather', JSON.stringify(data));
+                // save timestamp
+                localStorage.setItem('weather_timestamp', Date.now()); // Date.now() returns the number of milliseconds since January 1, 1970 00:00:00 UTC.
+
+            });
+    }
 }
