08fcb3db4831ef20fb2e820aad9f45806dc300a8
R0754975 <r0754975@student.thomasmore.be>
2022-10-16 20:18:17 +0200

    feat(edamam): add displayRecipe to show image link and title

--- src/Edamam.js
+++ src/Edamam.js
@@ -6,34 +6,26 @@ export default class Edamam {
 
     getRecipe(query) {
         const url = `https://api.edamam.com/api/recipes/v2/${query}?type=public&app_id=${this.appId}&app_key=${this.apiKey}`;
-        const weatherData = JSON.parse(localStorage.getItem('weather'));
-        let temp = weatherData.current.temp_c;
-
         fetch(url)
             .then(response => response.json())
             .then(data => {
 
-                console.log(temp);
-
-                if (temp < 20) {
-                    console.log(data);
-
-                    let image = data.recipe.image;
-                    
-                    let imagedom = document.querySelector(".edamam__image");
-                    imagedom.src = image;
-
-                    let imagelink = data.recipe.url;
-                    let imagelinkdom = document.querySelector(".edamam__link");
-                    imagelinkdom.href = imagelink;
-
-
-                }
-                else {
-                    console.log("It's too hot for soup!");
-                }
+                let image = data.recipe.image;
+                let imagelink = data.recipe.url;
+                let title = data.recipe.label;
+                this.displayRecipe(image, imagelink, title);
 
             });
+    }
+
+    displayRecipe(image, imagelink, title) {
+
+        let imagedom = document.querySelector(".edamam__image");
+        imagedom.src = image;
+        let imagelinkdom = document.querySelector(".edamam__link");
+        imagelinkdom.href = imagelink;
+        let titledom = document.querySelector(".edamam__title");
+        titledom.innerHTML = title;
     }
 
 }
