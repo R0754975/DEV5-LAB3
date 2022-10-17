38144459389a338d6e1ab681d29e8d12d834dcb2
R0754975 <r0754975@student.thomasmore.be>
2022-10-17 15:49:32 +0200

    fix: no longer have to refresh to display recipe

--- main.js
+++ main.js
@@ -1,33 +1,6 @@
 import './style.css'
 import Weather from './src/Weather.js'
-import Edamam from './src/Edamam';
 const weather = new Weather("39b326937d1b4715bad133205221610");
-const edamam = new Edamam("c8df7e6609724fa671ed4c1d44b2b4a7", "26d9a8b7");
 
-const weatherData = JSON.parse(localStorage.getItem('weather'));
-
-let temp = weatherData.current.temp_c;
-let edamamText = document.querySelector(".edamam__text");
-
-if(temp < 0) {
-  edamam.getRecipe("08ec480fab2bc79530c47ae3d8f4269b");
-  edamamText.innerHTML = "Brrrr! It's really cold outside. Here's a recipe for a hot drink.";
-}
-else if(temp < 10) {
-  edamam.getRecipe("b373d8987afb5808439ff243c16ccb63");
-  edamamText.innerHTML = "It's pretty cold outside. Here's a recipe for some soup.";
-}
-else if(temp < 20) {
-  edamam.getRecipe("b79327d05b8e5b838ad6cfd9576b30b6");
-  edamamText.innerHTML = "It's an oke temperature outside. Have some chicken.";
-}
-else if(temp < 30) {
-  edamam.getRecipe("b20815eac65ee2fa5f848f56c16056dd");
-  edamamText.innerHTML = "It's hot outside. Let's keep it cool and just have a salad.";
-}
-else if(temp < 40) {
-  edamam.getRecipe("480fd56ab4d71c204c2b75e16edbbd21");
-  edamamText.innerHTML = "It's really hot outside. Here's a recipe for some frozen treats.";
-}
 
 
--- src/Weather.js
+++ src/Weather.js
@@ -1,3 +1,7 @@
+import Edamam from './Edamam';
+
+const edamam = new Edamam("c8df7e6609724fa671ed4c1d44b2b4a7", "26d9a8b7");
+
 export default class Weather {
     constructor(api_key) {
         this.apiKey = api_key;
@@ -36,6 +40,32 @@ export default class Weather {
                 localStorage.setItem('weather', JSON.stringify(data));
                 // save timestamp
                 localStorage.setItem('weather_timestamp', Date.now()); // Date.now() returns the number of milliseconds since January 1, 1970 00:00:00 UTC.
+
+                let temp = data.current.temp_c;
+                let edamamText = document.querySelector(".edamam__text");
+
+                if(temp < 0) {
+                    edamam.getRecipe("08ec480fab2bc79530c47ae3d8f4269b");
+                    edamamText.innerHTML = "Brrrr! It's really cold outside. Here's a recipe for a hot drink.";
+                  }
+                  else if(temp < 10) {
+                    edamam.getRecipe("b373d8987afb5808439ff243c16ccb63");
+                    edamamText.innerHTML = "It's pretty cold outside. Here's a recipe for some soup.";
+                  }
+                  else if(temp < 20) {
+                    edamam.getRecipe("b79327d05b8e5b838ad6cfd9576b30b6");
+                    edamamText.innerHTML = "It's an oke temperature outside. Have some chicken.";
+                  }
+                  else if(temp < 30) {
+                    edamam.getRecipe("b20815eac65ee2fa5f848f56c16056dd");
+                    edamamText.innerHTML = "It's hot outside. Let's keep it cool and just have a salad.";
+                  }
+                  else if(temp < 40) {
+                    edamam.getRecipe("480fd56ab4d71c204c2b75e16edbbd21");
+                    edamamText.innerHTML = "It's really hot outside. Here's a recipe for some frozen treats.";
+                  }
+
+
 
             });
     }
